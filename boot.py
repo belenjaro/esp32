0ba196afbd690ef9232afaba281f74707a0d1549
Hector <hectordesosa.01@gmail.com>
2024-03-19 16:11:45 -0300

    wifi

--- boot.py
+++ boot.py
@@ -4,6 +4,6 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 if not wlan.isconnected():
     print('conectando a la red...')
-    wlan.connect('SSID', 'PASSWORD')
+    wlan.connect('SalaTI', 'edupublica')
     while not wlan.isconnected():
         pass
