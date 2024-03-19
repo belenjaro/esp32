26497744e7a66b2824bf9106d8a6487000a12e31
Hector <hectordesosa.01@gmail.com>
2024-03-19 16:34:48 -0300

    otro cambio

--- boot.py
+++ boot.py
@@ -1,1 +1,9 @@
-# nada
+import network
+from settings import SSID, password
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+if not wlan.isconnected():
+    print('conectando a la red...')
+    wlan.connect('SSID', 'password')
+    while not wlan.isconnected():
+        pass
