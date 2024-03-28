4b6d3e31c77b25cc1fe1a14b853d88a8a04d71b0
Hector <hectordesosa.01@gmail.com>
2024-03-28 18:32:03 -0300

    base de datos

--- main.py
+++ main.py
@@ -15,13 +15,15 @@
 
 from mqtt_as import MQTTClient
 from mqtt_local import config
-import asyncio
+import uasyncio as asyncio
 #import uasyncio as asyncio
 import dht, machine
 import json
 import ubinascii
 import unique_id
+import uos as os
 from settings import BROKER
+import btree
 #temperatura
 #humedad
 #setpiont es flotante 
@@ -48,28 +50,33 @@ led = machine.Pin(27, machine.Pin.OUT)
 led.value(1)# activo en bajo
 #el led inicialmente esta apagado
 
-#rele pin 12
+#rele
 rele= machine.Pin(12, machine.Pin.OUT)
 rele.value(1) #activo en bajo
 #esta apagado
 
 bandestello=False
 
+#modificacion de parametros
 def sub_cb(topic, msg, retained):
     #redibo y decodifico
     topicodeco=topic.decode()
     msgdeco=msg.decode()
-    #muestro el potico y el mensaje
+    global bandestello
+    cambio=False    
+    #muestro el topico y el mensaje
     print('Topic = {} -> Valor = {}'.format(topicodeco, msgdeco))
     #condiciones para los topicos 
     if topicodeco == 'setpoint':
         try:
             parametros['setpoint']=float(msgdeco)
+            cambio=True
         except OSError:
             print("ERROR, debe ser flotante")
     elif topicodeco == "periodo":
         try:
             parametros['periodo']=float(msgdeco)
+            cambio=True
         except OSError:
             print("ERROR, debe ser flotante")
     elif topicodeco =="destello" :
@@ -82,10 +89,12 @@ def sub_cb(topic, msg, retained):
         banmodo= msgdeco.lower()
         if banmodo == "manual":
             parametros['modo']=banmodo
+            cambio=True
             print("Modo manual")
         elif banmodo == "auto":
             parametros['modo']=banmodo
             print("Modo automatico")
+            cambio=True
         else:
             print("Modo incorrecto debe ser manual/auto")
     elif topicodeco== "rele":
@@ -100,11 +109,13 @@ def sub_cb(topic, msg, retained):
     else:
         print("No hay dicho topico")
     
+    if cambio is True:
+        escribir_db()
 async def wifi_han(state):
     print('Wifi is ', 'up' if state else 'down')
     await asyncio.sleep(1)
 
-# If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
+#subcripcion a los topicos 
 async def conn_han(client):
     await client.subscribe('setpoint', 1)
     await client.subscribe('periodo', 1)
@@ -125,50 +136,75 @@ async def monitoreo():
             else:
                 parametros['rele']='OFF'
                 rele.value(1)#apaga rele
-        await asyncio.sleep(5)
+        await asyncio.sleep(7)
 
-#destello del led
 #destellará por unos segundos 
 #cuando reciba la orden "destello" por mqtt.
 async def destello():
+    global bandestello
+    N=20
     while True:
         if bandestello==True:
-            led.value(0)
-            await asyncio.sleep(0.2)
-            led.value(1)
-            await asyncio.sleep(0.2)
-            led.value(0)
-            await asyncio.sleep(0.2)
-            led.value(1)
-        await asyncio.sleep(1)
-
+            for i in range(N):
+                led.value(0)
+                await asyncio.sleep(0.5)
+                led.value(1)
+                await asyncio.sleep(0.5)
+            bandestello=False
+            
 async def main(client):
     await client.connect()
-    await asyncio.sleep(2)  # Give broker time
+    await asyncio.sleep(2)  # Esperar para dar tiempo al broker
     while True:
         try:
-            await client.publish(f"iot/{CLIENT_ID}", json.dumps(parametros),qos=1)
-        except OSError:
-            print("Fallo del sensor")
-        await asyncio.sleep(parametros['periodo'])  # Broker is slow
-        
+            await client.publish(f"iot/{CLIENT_ID}", json.dumps(parametros), qos=1)
+        except OSError as e:
+            print(f"Fallo al publicar: {e}")
+        await asyncio.sleep(parametros['periodo'])  # Esperar según el periodo definido
+  
+async def task(client):
+    # Ejecutar monitoreo(),destello() y main() en paralelo
+    monitoreo_task = asyncio.create_task(monitoreo())
+    destello_task = asyncio.create_task(destello())
+    main_task=asyncio.create_task(main(client))
+    await asyncio.gather(monitoreo_task, destello_task, main_task)
+
+def escribir_db():
+    with open("db", "w+b") as f:
+        db = btree.open(f)
+        db[b'setpoint'] = b"{}".format(str(parametros['setpoint']))
+        db[b'periodo'] = b"{}".format(str(parametros['periodo']))
+        db[b'modo'] = b"{}".format(str(parametros['modo']))
+        db.flush()
+        db.close()
+    
+def leer_db():
+    with open("db", "r+b") as f:
+        db = btree.open(f)
+        parametros['setpoint']=db[b'setpoint']
+        parametros['periodo']=db[b'periodo']
+        parametros['modo']=db[b'modo']
+        db.flush()
+        db.close()
+
 # Define configuration
 config['subs_cb'] = sub_cb
 config['connect_coro'] = conn_han
 config['wifi_coro'] = wifi_han
 config['ssl'] = True
 
-
+if 'db' not in os.listdir():
+    print("Creando base de datos...")
+    escribir_db()
+else:
+    print("Leyendo base de datos...")
+    leer_db()
 # Set up client
 MQTTClient.DEBUG = True  # Optional
 client = MQTTClient(config)
 try:
-    asyncio.run(main(client))
+    asyncio.run(task(client))
 finally:
     client.close()
     asyncio.new_event_loop()
 
-
-#await asyncio.gather(destello(),monitoreo(),conn_han(),wifi_han())
-#await asyncio.create_task(destello(),monitoreo(),conn_han(),wifi_han())
-#
