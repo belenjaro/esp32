e5b72e57f08bf9da2abd38739b9668ab5cb2934e
HectorDeSosa <hectordesosa.01@gmail.com>
2024-06-16 17:55:25 -0300

    cambio_para conexion

--- boot.py
+++ boot.py
@@ -1,9 +1,9 @@
 import network
-from settings import SSID, password
+from settings import SSID, PASS
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 if not wlan.isconnected():
     print('conectando a la red...')
-    wlan.connect('SSID', 'password')
+    wlan.connect(SSID, PASS)
     while not wlan.isconnected():
         pass
---
+++ settings.py
@@ -0,0 +1,11 @@
+BROKER='hectoriot2024.duckdns.org'
+#SSID='Sala-TI'
+#PASS='edupublica'
+#SSID='Wi-Fio LabEtronica'
+#PASS='wds2008fio'
+SSID='MI-WIFI'
+#PASS='jaroszuk2020'
+password='jaroszuk2020'
+USR_MQTT='hector'
+PASS_MQTT='hectoriot'
+PUERTO_MQTTS=10862
